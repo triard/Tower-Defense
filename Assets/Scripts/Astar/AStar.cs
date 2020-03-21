025e8d7e2f359e5934f5652c5de3c5e7ade25b00
triard <triard78@gmail.com>
2020-03-21 09:25:23 +0700

    6.1 creating nodes

---
+++ Assets/Scripts/Astar/AStar.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class AStar
+{
+    public static Dictionary<Point, Node> nodes;
+    private static void CreateNodes()
+    {
+        nodes = new Dictionary<Point, Node>();
+        foreach(TileScript tile in LevelManajer.Instance.Tiles.Values)
+        {
+            nodes.Add(tile.GridPosition, new Node(tile));
+        }
+    }
+}
