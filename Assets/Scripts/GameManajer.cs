45b5a9c91a19599ac908364affdb6a9aad1653c5
triard <triard78@gmail.com>
2020-03-21 08:23:37 +0700

    5.6 currency

--- Assets/Scripts/GameManajer.cs
+++ Assets/Scripts/GameManajer.cs
@@ -1,16 +1,34 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameManajer : Singleton<GameManajer>
 {
 
   
     public TowerButton ClikedBtn { get; set; }
+    
+    private int currency;
+
+    public int Currency {
+        get
+        {
+           return currency;
+        }
+        set
+        {
+            this.currency = value;
+            this.currencyTxt.text = value.ToString() + " <color=lime>Coins</color>";
+        }
+    }
+
+    [SerializeField]
+    private Text currencyTxt;
     // Start is called before the first frame update
     void Start()
     {
-        
+        Currency = 5; 
     }
 
     // Update is called once per frame
